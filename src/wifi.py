1ce08ca588bb639e91fdcabfa131bfce7cba0292
Julian Jark <juliuxu@gmail.com>
2019-12-31 10:23:05 +0100

    disable wifi ap

--- src/wifi.py
+++ src/wifi.py
@@ -3,13 +3,18 @@ import network
 import config
 
 _sta_if = None
+_ap_if = None
 
 
 def setup():
-    global _sta_if
+    global _sta_if, _ap_if
     _sta_if = network.WLAN(network.STA_IF)
     _sta_if.active(True)
     _sta_if.connect(config.get()["wifi_essid"], config.get()["wifi_password"])
+
+    # TODO: Enable config interface
+    _ap_if = network.WLAN(network.AP_IF)
+    _ap_if.active(False)
 
 
 def isconnected():
